22725522f0a37d962d69a8adcd11d8fbe2d9498e
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-02-14 16:12:08 -0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CadastroProdutosComponent } from './components/pages/cadastro-produtos/cadastro-produtos.component';
+import { ConsultaProdutosComponent } from './components/pages/consulta-produtos/consulta-produtos.component';
+import { EdicaoProdutosComponent } from './components/pages/edicao-produtos/edicao-produtos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pages/cadastro-produtos',
+        component: CadastroProdutosComponent
+    },
+    {
+        path: 'pages/consulta-produtos',
+        component: ConsultaProdutosComponent
+    },
+    {
+        path: 'pages/edicao-produtos',
+        component: EdicaoProdutosComponent
+    }
+];
