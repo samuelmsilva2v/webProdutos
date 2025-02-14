a225999b4c1a08eee1b6a1dd89de90bf93ac4628
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-02-14 16:09:48 -0300

    created components, navbar

---
+++ src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-produtos',
+  standalone: true,
+  imports: [],
+  templateUrl: './consulta-produtos.component.html',
+  styleUrl: './consulta-produtos.component.css'
+})
+export class ConsultaProdutosComponent {
+
+}
