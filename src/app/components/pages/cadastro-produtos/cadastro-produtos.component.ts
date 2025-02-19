0a2330ab03dac29357234edee0aa297a007a3093
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-02-19 17:34:13 -0300

    cadastro produtos onsubmit

--- src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-produtos',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.css'
@@ -26,5 +29,24 @@ export class CadastroProdutosComponent {
         }
       });
   }
+  
+  form = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    preco: new FormControl('', Validators.required),
+    quantidade: new FormControl('', Validators.required),
+    categoriaId: new FormControl('', Validators.required)
+  });
 
+  onSubmit() {
+
+    this.http.post('http://localhost:8080/api/produtos/cadastrar', this.form.value, {responseType: 'text'})
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (e) => {
+          console.error(e.error);
+        }
+      });
+  }
 }
