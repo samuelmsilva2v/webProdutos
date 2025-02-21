ed4f3e62d8bc2b146cd141b48916dc79e42785db
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-02-21 14:22:26 -0300

    consulta produtos

--- src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
+++ src/app/components/pages/consulta-produtos/consulta-produtos.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-consulta-produtos',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './consulta-produtos.component.html',
   styleUrl: './consulta-produtos.component.css'
 })
 export class ConsultaProdutosComponent {
 
+  produtos: any[] = [];
+
+  constructor (private http: HttpClient) { }
+
+  form = new FormGroup({
+    nome: new FormControl('')
+  });
+
+  onSubmit() {
+    this.http.get('http://localhost:8080/api/produtos/consultar/' + this.form.value.nome)
+      .subscribe({
+        next: (data) => {
+          this.produtos = data as any[];
+        }
+      });
+  }
 }
