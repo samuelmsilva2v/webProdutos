0e847ab9bf1f7d84a05b222df34928d8ae280f3d
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-01 14:50:28 -0300

    dashboard

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { endpoints } from '../../../configurations/environment';
+import { Chart, ChartModule } from 'angular-highcharts';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    ChartModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
@@ -16,21 +18,42 @@ export class DashboardComponent {
 
   // Atributos
   dados: any[] = [];
+  grafico: Chart = new Chart();
 
   // Construtores
   constructor(private http: HttpClient) { }
 
   // Método executado ao abrir a página
   ngOnInit() {
-    
+
     // Executando uma requisição GET para a API
     this.http.get(endpoints.dashboard_categorias)
-    .subscribe({
-      next: (data) => {
-        this.dados = data as any[];
-      }
-    })
+      .subscribe({
+        next: (data) => {
+          this.dados = data as any[];
+          let conteudo: any[] = [];
+          this.dados.forEach(item => {
+            conteudo.push([item.nomeCategoria, item.qtdProdutos]);
+          });
 
-  
+          this.grafico = new Chart({
+            chart: { type: 'pie' },
+            title: { text: 'Quantidade de produtos por categoria.' },
+            subtitle: { text: "Total de produtos separados por tipo de categoria." },
+            plotOptions: {
+              pie: {
+                innerSize: '50%',
+                dataLabels: { enabled: true }
+              }
+            },
+            series: [{
+              data: conteudo, type: 'pie',
+              name: 'Categorias'
+            }],
+            legend: { enabled: false },
+            credits: { enabled: false }
+          });
+        }
+      });
   }
 }
